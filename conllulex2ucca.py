f8deb729de26d0f7a9624584f0d2205e49dae5de
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-02-17 20:52:49 +0200

    Label scene nouns as P even if part of MWE

--- conllulex2ucca.py
+++ conllulex2ucca.py
@@ -244,7 +244,7 @@ class ConllulexToUccaConverter:
         mapped = [UD_TO_UCCA.get(basic_deprel, deprel)]
         # Use supersenses to find Scene-evoking phrases and select labels accordingly
         if Categories.Center in mapped:
-            if node.is_scene_evoking() or node.is_scene_noun() and node.swe:
+            if node.is_scene_evoking() or node.is_scene_noun():
                 mapped = [Categories.Process]
             elif node.lexcat == "ADJ" or node.ss in ("n.STATE", "n.ATTRIBUTE", "n.FEELING"):
                 mapped = [Categories.State]
